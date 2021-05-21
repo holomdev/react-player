4003938dda660e31a9db1b05b78cc780efd4a953
holomdev <holom.dev@gmail.com>
2021-05-21 09:58:58 -0500

    creating init components

---
+++ src/components/Song.js
@@ -0,0 +1,11 @@
+const Song = () => {
+  return (
+    <div className="song-container">
+      <h1>Picture</h1>
+      <h2>Song Name</h2>
+      <h1>Artist</h1>
+    </div>
+  );
+};
+
+export default Song;
