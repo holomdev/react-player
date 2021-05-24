79f63ccd8af82212dd869911784e71b60ac7e4cc
holomdev <holom.dev@gmail.com>
2021-05-24 13:46:13 -0500

    menu icon

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,13 +1,12 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMusic } from "@fortawesome/free-solid-svg-icons";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 const Nav = ({ libraryStatus, setLibraryStatus }) => {
   return (
     <nav>
       <h1>Waves</h1>
       <button onClick={() => setLibraryStatus(!libraryStatus)}>
-        <FontAwesomeIcon icon={faMusic} />
-        Menu Songs
+        <FontAwesomeIcon size="2x" icon={faBars} />
       </button>
     </nav>
   );
